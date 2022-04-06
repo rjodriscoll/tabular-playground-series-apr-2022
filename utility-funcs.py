257ced8e8083b0a6b11a69df2ba1237633a3746e
rjodriscoll <48929734+rjodriscoll@users.noreply.github.com>
2022-04-06 22:23:44 +0100

    Kaggle Notebook | utility_funcs | Version 2

---
+++ utility-funcs.py
@@ -0,0 +1,65 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "56ced541",
+   "metadata": {
+    "_cell_guid": "b1076dfc-b9ad-4769-8c92-a6c4dae69d19",
+    "_uuid": "8f2839f25d086af736a60e9eeb907d3b93b6e0e5",
+    "execution": {
+     "iopub.execute_input": "2022-04-06T21:23:31.846351Z",
+     "iopub.status.busy": "2022-04-06T21:23:31.845698Z",
+     "iopub.status.idle": "2022-04-06T21:23:31.847499Z",
+     "shell.execute_reply": "2022-04-06T21:23:31.848106Z"
+    },
+    "papermill": {
+     "duration": 0.013633,
+     "end_time": "2022-04-06T21:23:31.848389",
+     "exception": false,
+     "start_time": "2022-04-06T21:23:31.834756",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": [
+    "def test():\n",
+    "    print('kaggle utility script')"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.12"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 9.357676,
+   "end_time": "2022-04-06T21:23:32.460909",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2022-04-06T21:23:23.103233",
+   "version": "2.3.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
