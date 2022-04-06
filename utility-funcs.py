a06ca65aa2dfd93e6660b46d925f48cf201d0051
rjodriscoll <48929734+rjodriscoll@users.noreply.github.com>
2022-04-06 22:25:58 +0100

    Kaggle Notebook | utility_funcs | Version 3

--- utility-funcs.py
+++ utility-funcs.py
@@ -3,21 +3,21 @@
   {
    "cell_type": "code",
    "execution_count": 1,
-   "id": "56ced541",
+   "id": "7e72e0d4",
    "metadata": {
     "_cell_guid": "b1076dfc-b9ad-4769-8c92-a6c4dae69d19",
     "_uuid": "8f2839f25d086af736a60e9eeb907d3b93b6e0e5",
     "execution": {
-     "iopub.execute_input": "2022-04-06T21:23:31.846351Z",
-     "iopub.status.busy": "2022-04-06T21:23:31.845698Z",
-     "iopub.status.idle": "2022-04-06T21:23:31.847499Z",
-     "shell.execute_reply": "2022-04-06T21:23:31.848106Z"
+     "iopub.execute_input": "2022-04-06T21:25:45.856291Z",
+     "iopub.status.busy": "2022-04-06T21:25:45.855392Z",
+     "iopub.status.idle": "2022-04-06T21:25:45.859257Z",
+     "shell.execute_reply": "2022-04-06T21:25:45.858578Z"
     },
     "papermill": {
-     "duration": 0.013633,
-     "end_time": "2022-04-06T21:23:31.848389",
+     "duration": 0.01887,
+     "end_time": "2022-04-06T21:25:45.859437",
      "exception": false,
-     "start_time": "2022-04-06T21:23:31.834756",
+     "start_time": "2022-04-06T21:25:45.840567",
      "status": "completed"
     },
     "tags": []
@@ -49,14 +49,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 9.357676,
-   "end_time": "2022-04-06T21:23:32.460909",
+   "duration": 12.479188,
+   "end_time": "2022-04-06T21:25:48.925405",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2022-04-06T21:23:23.103233",
+   "start_time": "2022-04-06T21:25:36.446217",
    "version": "2.3.3"
   }
  },
